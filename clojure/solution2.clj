08ff7e33b7e028d439341505101286e054f29983
sungsik.kim <skystar819@gmail.com>
2017-09-11 23:56:44 +0900

    solve problem2

---
+++ clojure/solution2.clj
@@ -0,0 +1,9 @@
+(defn fib [f s]
+  (lazy-seq (cons f (fib s (+ f s)))))
+
+(->> (take 50 (fib 0 1))
+     (filter #(< % 4000000))
+     (filter #(= (mod % 2) 0))
+     (reduce + ))
+
+(reduce + (filter #(= (mod % 2) 0) (filter #(< % 4000000) (take 50 (fib 0 1)))))
