145ade86906eb636d33ed4e9dad30b453a0607a2
sungsik.kim <skystar819@gmail.com>
2021-10-08 07:37:45 +0900

    소스 코드 정리

--- src/problem5.clj
+++ src/problem5.clj
@@ -1,33 +1,4 @@
-(ns problem5)
+(ns problem5
+  (:require [utils :as u]))
 
-;; 약수 구하기 - 사용 안함
-(defn reminders [n]
-  (let [max (inc (Math/sqrt n))]
-    (->>
-     (filter #(= (rem n %) 0) (range 1 max))
-     (reduce #(conj %1 %2 (quot n %2))
-             [])
-     (set)
-     (sort))))
-
-;; 최대 공약수
-(defn gcd [a b] 
-  (loop [r (rem a b)
-         a b]
-         (if (zero? r) a
-             (recur (rem a r) r))))
-
-;; 절대값
-(defn abs [n]
-  (max n (-' n)))
-
-;; 최소 공배수
-(defn lcm [a b]
-  (quot (abs (* a b)) (gcd a b)))
-
-;; 순차적인 n개의 숫자의 최소공배수 구하기
-(defn n-lcm [a b]
-  (reduce #(if (>= (lcm %1 %2) %1) (lcm %1 %2) %2)
-          (range a (inc b))))
-
-(n-lcm 1 19);; => 232792560
+(u/n-lcm 1 19);; => 232792560
