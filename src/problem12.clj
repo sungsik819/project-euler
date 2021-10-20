219d29a986dc24d32e97eb111909c5a2c3448bbe
sungsik.kim <skystar819@gmail.com>
2021-10-21 05:57:59 +0900

    12번 문제 공식 적용하여 정리

--- src/problem12.clj
+++ src/problem12.clj
@@ -4,7 +4,6 @@
 ;; 삼각수의 성질
 ;; n번째 수를 구하기 위해서는 이전 숫자에 n을 더한다.
 ;; 예) 2번째 수를 구하려면 1 + 2로 3이 된다.
-
 (defn triangle-number [end]
   (->>
    (range 1 (inc end))
@@ -14,10 +13,29 @@
 (defn triangle-number2 [end]
   (quot (* end (+ end 1)) 2))
 
+(defn count-divisor [n]
+  (count (u/divisor n)))
+
+;; 삼각수 약수의 갯수 구하기
+(defn count-triangle-divisor [n]
+  (if (zero? (rem n 2))
+    (* (count-divisor (quot n 2)) (count-divisor (inc n)))
+    (* (count-divisor n) (count-divisor (quot (inc n) 2)))))
+
+;; 약수가 n개인 삼각수 중 제일 작은 수
+(defn triangle-divisor [n]
+  (let [[a] (take 1 (filter #(when (<= n (count-triangle-divisor %)) %) (range)))]
+    (* a (quot (inc a) 2))))
+
+(u/sectime (triangle-divisor 10))
+
+
+(map #(vector % (triangle-number2 %)) (range 12370 12380))
+
 ;; 76576500
 ;; 오래 걸림
 ;; 범위를 맞춰서 찾느라 몇번 틀림
 (->>
-            (map #(triangle-number %) (range 10000 100000))
+            (map #(triangle-number2 %) (range 12370 12380))
             (map #(vector % (count (u/divisor %))))
             (filter #(<= 500 (last %))))
